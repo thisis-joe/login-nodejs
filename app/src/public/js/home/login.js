df03bea515ead87b1dc9026801b18cf8e59fb025
joseph <wdg0434@gmail.com>
2022-10-01 01:08:45 +0900

    DOM으로 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,1 +1,16 @@
-console.log("hello")
+"use strict";
+
+const id = document.querySelector("#id"),
+    psword=document.querySelector("#psword"),
+    loginBtn=document.querySelector("button");
+
+loginBtn.addEventListener("click",login);
+
+function login(){
+    const req={
+        id : id.value,
+        psword : psword.value,
+    }
+    console.log(req);
+    //fetch(); //서버에 전달
+}

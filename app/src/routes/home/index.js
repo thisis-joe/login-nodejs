cf7561eacb64b0983d61450bc5b2779c460777f5
joseph <wdg0434@gmail.com>
2022-10-06 06:11:10 +0900

    회원가입 페이지 생성

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -17,6 +17,8 @@ const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
+router.get("/register",ctrl.output.register);
 router.post("/login", ctrl.process.login); //프론트엔드가 전달한 로그인데이터를 받아서 로그인 기능을 처리할 부분
+
 
 module.exports = router; //외부에서 router변수 사용할 수 있게 내보냄. app.js파일에서 써야하니까.
